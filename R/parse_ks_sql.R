380bd644fe9c04aa761accdb261c3482e705acd3
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2019-07-04 17:48:02 +0800

    add glue

--- R/parse_ks_sql.R
+++ R/parse_ks_sql.R
@@ -9,21 +9,21 @@ parse_ks_sql <- function(verbose = 0,
                          y_hat = "pred",
                          y = "target",
                          table = "opd.test_pred_table") {
-    raw_text <- glue(
-        "select {y_hat}, {y},
+  raw_text <- glue::glue(
+    "select {y_hat}, {y},
           sum(y=1) over (order by y_hat rows between current row and unbounded following) as tp,
           sum(y=0) over (order by y_hat rows between current row and unbounded following) as fp,
           sum(y=1) over (order by y_hat rows between unbounded preceding and 1 preceding) as fn,
           sum(y=0) over (order by y_hat rows between unbounded preceding and 1 preceding) as tn
       from {table}"
-    )
+  )
 
-    sqltext <- if (verbose == 0) {
-        raw_text
+  sqltext <- if (verbose == 0) {
+    raw_text
 
-    } else if (verbose == 1) {
-        glue(
-            "with a as (
+  } else if (verbose == 1) {
+    glue::glue(
+      "with a as (
        {raw_text}
        )
        select {y_hat}, {y},
@@ -32,20 +32,20 @@ parse_ks_sql <- function(verbose = 0,
           tp/(tp+fn) - fp/(fp+tn) as ks
        from a
        "
-        )
+    )
 
-    } else if (verbose == 2) {
-        glue("with a as (
+  } else if (verbose == 2) {
+    glue::glue("with a as (
        {raw_text}
        )
        max(tp/(tp+fn) - fp/(fp+tn)) as ks
        from a
        ")
-    } else {
-        "stop use verbose in [0,2]"
-    }
+  } else {
+    "stop use verbose in [0,2]"
+  }
 
-    cat(sqltext)
-    clipr::write_clip(sqltext, allow_non_interactive = TRUE)
-    cat("The SQL text is on your clipboard.")
+  cat(sqltext)
+  clipr::write_clip(sqltext, allow_non_interactive = TRUE)
+  cat("The SQL text is on your clipboard.")
 }
